1810c731007ab7c9ce3d38acd8296aaf9675cb35
salihbezai <salihbezai98@gmail.com>
2026-01-17 20:17:11 +0100

    update: add filter buttons and download button report for manage task page

--- frontend/src/pages/Admin/ManageTasks.tsx
+++ frontend/src/pages/Admin/ManageTasks.tsx
@@ -30,10 +30,18 @@ const ManageTasks = () => {
     <div>
       <div className='flex justify-between items-center mb-4'>
         <h1 className='text-2xl font-bold'>My Tasks</h1>
-        {/* Filter tasks on status */}
-        <div>
-          Pednding
+        {/* Filter tasks on status nav  */}
+        <div className='flex'>
+          <button className='text-gray px-4 py-2 rounded mr-2 cursor-pointer'>All</button>
+          <button className='text-gray px-4 py-2 rounded mr-2 cursor-pointer'>Completed</button>
+          <button className='text-gray px-4 py-2 rounded mr-2 cursor-pointer'>In Progress</button>
+          <button className='text-gray px-4 py-2 rounded mr-2 cursor-pointer'>Pending</button>
+          {/* download report button */}
+          <div className='ml-4'>
+          <button className='bg-blue-500 text-white px-4 py-2 rounded cursor-pointer'>Download Report</button>
+          </div>
         </div>
+  
       </div>
     <div className='w-full grid md:grid-cols-3 gap-2 bg-gray-100'>
 
